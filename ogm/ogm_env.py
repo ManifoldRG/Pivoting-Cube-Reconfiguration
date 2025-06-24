59a7a2cb6a7bd2cd6d4f874303eac37c4a835bf0
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2025-06-24 22:00:45 +0530

    ogm ogm_env wrapper added

---
+++ ogm/ogm_env.py
@@ -0,0 +1,44 @@
+# import gymnasium as gym
+# from gymnasium import spaces
+import numpy as np
+from ogm.occupancy_grid_map import OccupancyGridMap
+
+"""
+Environment wrapper for OGM
+"""
+class OGMEnv:
+    def __init__(self, step_cost = -0.01, max_steps = None):
+        self.step_cost = step_cost
+        self.max_steps = max_steps 
+        self.steps_token = 0
+        self.ogm = OccupancyGridMap
+
+    def reset(self, initial_config, final_config):
+        self.ogm = OccupancyGridMap(initial_config, final_config, len(initial_config))
+        self.steps_taken = 0
+        return self.ogm.get_observation()
+    
+    def step(self, action):
+        if self.ogm is None:
+            raise Exception("Environment not set. call reset function")
+
+        module, pivot = action
+        self.ogm.take_action(module, pivot)
+        self.steps_taken += 1
+        done = self.ogm.check_final()
+        reward = 1.0 if done else self.step_cost
+
+        ## This will allow ogm to stop at some point?? Not sure if will be useful
+        if self.max_steps is not None and self.steps_taken >= self.max_steps:
+            done = True
+
+        observation = self.get_observation()
+        info = {'step': self.steps}
+        return observation, reward, done, info
+    
+    def get_observation(self):
+
+        if self.ogm is None:
+            raise Exception("Environment not set. call reset function")
+
+        return np.copy(self.ogm.curr_grid_map)
