0f003e37e89607ed29775c274f20624c91e7808d
Parth2412 <parthnpatil06@gmail.com>
2026-01-12 18:01:26 +0530

    doc(four band reduction + GRU): Add example implementation of GRU with four band reduction

--- feature_extractors/recurrent.py
+++ feature_extractors/recurrent.py
@@ -2,7 +2,7 @@ import gymnasium
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence
-from feature_extractors.base import K_Local_FeatureExtractor
+from feature_extractors.base import K_Local_FeatureExtractor, _4BandFeatureExtractor
 from utils.positional_embeddings import sinusoidal_embedding
 
 class RecurrentFeatureExtractor(K_Local_FeatureExtractor):
@@ -20,4 +20,22 @@ class RecurrentFeatureExtractor(K_Local_FeatureExtractor):
             "agent_id": lambda agent_ids,num_agents: sinusoidal_embedding(self.agent_id_dim, agent_ids)
         }
 
+
+# Example of implementing 4 band feature reduction and applying it to GRU
+"""
+class RecurrentFeatureExtractor(_4BandFeatureExtractor):
+    def __init__(self, observation_space: gymnasium.spaces.Dict, embedded_dim = 256, agent_id_dim = 64):
+        self.agent_id_dim = agent_id_dim  # Dimension for sinusoidal embedding
+        self.embedded_dim = embedded_dim
+        super().__init__(observation_space=observation_space,features_dim=2*self.embedded_dim + self.agent_id_dim)
+
+        self.encoder_model = nn.GRU(input_size=4,hidden_size=self.embedded_dim,dtype=torch.float32, batch_first=True)
+
+        self.encoder = lambda observation,num_agents : self.encoder_model(pack_padded_sequence(self.four_band_reducer(observation,num_agents), num_agents, batch_first=True, enforce_sorted=False))[1].squeeze(0)
+        self.extractors = {
+            "current_config": self.encoder,
+            "final_config": self.encoder,
+            "agent_id": lambda agent_ids,num_agents: sinusoidal_embedding(self.agent_id_dim, agent_ids)
+        }
+"""
 
